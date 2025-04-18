949e9ab4aaa02d104043f9c71cd6ed232a413764
natsu <alaoui.hamza2002@gmail.com>
2025-04-18 22:46:49 +0100

    first commit

---
+++ src/main/java/com/nli/tagwise/TagWiseApplication.java
@@ -0,0 +1,13 @@
+package com.nli.tagwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TagWiseApplication.class, args);
+    }
+
+}
